81445e1741fcea71801aa424cb7cc964e88551b2
Andries Hiemstra <andries@fam-hiemstra.nl>
2021-04-09 21:33:25 +0200

    wip

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// use warp as web server
+// create a parallelRuntime
+// impl setParallel
+// project descriptors revamp (attention for clientside code)
+// lazy init eventdriven serverside projects
+// typescript! macro's! ifdef! profiling!
+
+fn main(){
+    println!("hello");
+}
